b5eb726143b0b7b1def924042ff8f08984ab3756
Rishabh Marya <rishabhmarya@Rishabhs-MacBook-Pro.local>
2013-10-24 18:03:58 -0500

    permutations function complete

--- perm.js
+++ perm.js
@@ -1,5 +1,20 @@
 function permute(a) {
-	
+	var perms = [], rec = [];
+	if (a.length > 2)
+		rec = permute(a.slice(1));
+	else {
+		return [a, a.reverse()];
+	}
+
+	rec.forEach(function (word) {
+		for(var i=0; i<word.length; i++) {
+			perms.push(word.slice(0,i).concat([a[0]]).concat(word.slice(i, word.length)));
+		}
+		perms.push(word.concat([a[0]]))
+	})
+
+	return perms;
+
 }
-console.log(permute([1,2,3]))
+console.log(permute([1,2,3,4,5]))
 
