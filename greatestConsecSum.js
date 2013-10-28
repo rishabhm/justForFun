f173bd76bbc93d0cd8c0cef6fa5338a83d6e3bb2
Rishabh Marya <rishabhmarya@Rishabhs-MacBook-Pro.local>
2013-10-28 17:39:44 -0500

    kadane algorithm

--- greatestConsecSum.js
+++ greatestConsecSum.js
@@ -1,3 +1,20 @@
 /*
 	Find the greatest sum of consecutive integers in an array
+	Kadane's Algorithm
 */
+
+function greatestConsecSum(arr) {
+	var sum=0, max=0;
+	arr.forEach(function (n) {
+		sum += n;
+		if (sum < 0) {
+			sum =0;
+		}
+		if (max < sum) {
+			max = sum;
+		}
+	})
+	return max;
+}
+
+console.log(greatestConsecSum([-2, -3, 4, -1, -2, 1, 5, -3, 5]))
