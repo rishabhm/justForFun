a29272121fa929e489742645ba0bc8d2183a569c
Rishabh Marya <rishabhmarya@Rishabhs-MacBook-Pro.local>
2013-10-28 17:47:46 -0500

    typos...

--- power.js
+++ power.js
@@ -1,11 +1,11 @@
 // Calculate a^b;
 
-function power3(a,b) {
+function power(a,b) {
     if (b==0)
         return 1;
     if (b==1)
         return a;
-    return a * power3(a, parseInt(b/2)) * power3(a,parseInt((b-1)/2));
+    return a * power(a, parseInt(b/2)) * power(a,parseInt((b-1)/2));
 }
 
-console.log(2,10); // We don't need to calculate this...
+console.log(power(2,10)); // We don't need to calculate this...
